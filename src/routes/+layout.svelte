4d18df53bf629ca185307326b205d7bf24fa63ee
Austin Jones <ajones53.aj@gmail.com>
2025-09-16 07:31:52 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="site-wrapper dark">
+	<div class="main-content-wrapper">
+		{@render children()}
+	</div>
+</div>
+
+<style lang="scss">
+	.site-wrapper {
+		display: flex;
+	}
+	.main-content-wrapper {
+		flex: 1;
+		max-width: 45rem;
+		width: 90%;
+		margin: 0 auto;
+		padding: 1rem;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { Canvas } from 'svelte-canvas';
+    import Ball from '$lib/components/Ball.svelte';
+
+    let balls = [
+        { color: 'tomato', x: 0.5, y: 0.333 },
+        { color: 'goldenrod', x: 0.333, y: 0.625 },
+        { color: 'mediumturquoise', x: 0.667, y: 0.625 }
+    ];
+
+    const reorder = (color) => {
+        balls = balls
+            .filter((c) => c.color !== color)
+            .concat(balls.filter((c) => c.color === color));
+    };
+</script>
+
+<h1 class="content-around text-3xl">Playing with the canvas lib</h1>
+
+<div>
+    <div class="canvas">
+        <Canvas layerEvents style="touch-action: none">
+            {#each balls as { color, x, y } (color)}
+                <Ball {color} {x} {y} onclick={() => reorder(color)} />
+            {/each}
+        </Canvas>
+    </div>
+</div>
+
+<style lang="scss">
+    div {
+        width: 100%;
+        aspect-ratio: 1;
+        justify-self: center;
+        border-radius: 0.5rem;
+        overflow: hidden;
+    }
+    .canvas {
+        background-color: var(--bg2-color);
+    }
+</style>
